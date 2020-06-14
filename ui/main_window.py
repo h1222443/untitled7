0266490d1127c5a1cb2dd89aea64e88326381c05
Administrator <857954176@qq.com>
2020-06-14 21:59:08 +0800

    d

--- ui/main_window.py
+++ ui/main_window.py
@@ -24,8 +24,7 @@ class Main_Window(QMainWindow):
         pass
     def hao(self):
         pass
-    def haoo(self):
-        pass
+
 
     def create_dock(self,widget_class:QWidget,name:str,area:int):
         widget = widget_class(self.event_engine)
